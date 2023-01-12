b639c1d1af795a0e90fde40af5cf7edda6b56c03
Adam Zakaria <adam.p.zakaria@gmail.com>
2023-01-12 02:04:37 -0500

    i

--- cn.py
+++ cn.py
@@ -1,16 +1,25 @@
 import pandas as pd
 
-# load the spreadsheet or dataframe
-df = pd.read_excel('your_spreadsheet.xlsx')
+# load the spreadsheets
+df0 = pd.read_excel('program_descriptions.xlsx')
+df1 = pd.read_excel('interventions.xlsx')
 
 # specify the column you want to iterate through
-column_name = 'column_name'
+column_name = 'program_descriptions'
 
 # iterate through the cells of the column
-for cell in df[column_name]:
+for cell in df0[column_name]:
     print(cell)
 
 # iterate through every cell
-for index, row in df.iterrows():
+for index, row in df1.iterrows():
     for cell in row:
         print(cell)
+
+
+def compare(type='bert'):
+    # bert is the default comparsion method
+    if type == 'bert':
+        return
+    if type == 'token_based':
+        return
