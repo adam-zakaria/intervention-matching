b95acecf7a4f74ec5b01a0945cc8ee3acb7bf63b
Adam Zakaria <adam.p.zakaria@gmail.com>
2023-01-12 02:13:06 -0500

    r'

--- cn.py
+++ cn.py
@@ -1,4 +1,20 @@
+# imports
+
 import pandas as pd
+
+# Helper functions
+
+
+def compare(type='bert'):
+    # bert is the default comparsion method
+    if type == 'bert':
+        return
+    if type == 'token_based':
+        return
+
+
+# Main execution
+
 
 # load the spreadsheets
 df0 = pd.read_excel('program_descriptions.xlsx')
@@ -10,16 +26,9 @@ column_name = 'program_descriptions'
 # iterate through the cells of the column
 for cell in df0[column_name]:
     print(cell)
-
-# iterate through every cell
-for index, row in df1.iterrows():
-    for cell in row:
-        print(cell)
-
-
-def compare(type='bert'):
-    # bert is the default comparsion method
-    if type == 'bert':
-        return
-    if type == 'token_based':
-        return
+    # iterate through every cell
+    for index, row in df1.iterrows():
+        for cel in row:
+            print(cel)
+            similarity = compare(cel)
+            # write similarity to the last column
